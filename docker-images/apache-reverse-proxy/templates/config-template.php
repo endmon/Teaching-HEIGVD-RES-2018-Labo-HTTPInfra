10626ee25075b73fb6aee487ab08487a155ff09f
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2018-06-15 06:50:36 +0200

    Added load balancing in reverse proxy

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,13 +1,39 @@
 <?php
-	$dynamic_app = getenv('DYNAMIC_APP');
-	$static_app = getenv('STATIC_APP');
+	$dynamic_app1 = getenv('DYNAMIC_APP1');
+	$dynamic_app2 = getenv('DYNAMIC_APP2');
+	$dynamic_app3 = getenv('DYNAMIC_APP3');
+	$static_app1 = getenv('STATIC_APP1');
+	$static_app2 = getenv('STATIC_APP2');
+	$static_app3 = getenv('STATIC_APP3');
 ?>
 <VirtualHost *:80>
+	ProxyRequests off
 	ServerName labo.res.ch
 
-	ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-	ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+	<Proxy balancer://express-cluster>
+		BalancerMember 'http://<?php print "$dynamic_app1"?>'
+		BalancerMember 'http://<?php print "$dynamic_app2"?>'
+		BalancerMember 'http://<?php print "$dynamic_app3"?>'
 
-	ProxyPass '/' 'http://<?php print "$static_app"?>/'
-	ProxyPassReverse '/' '<?php print "$static_app"?>/'
+		Require all Granted
+		ProxySet lbmethod=byrequests
+	</Proxy>
+
+	ProxyPass '/api/students/' 'balancer://express-cluster/'
+	ProxyPassReverse '/api/students/' 'balancer://express-cluster/'
+
+
+
+	<Proxy balancer://static-cluster>
+		BalancerMember 'http://<?php print "$static_app1"?>'
+		BalancerMember 'http://<?php print "$static_app1"?>'
+		BalancerMember 'http://<?php print "$static_app1"?>'
+
+	Require all Granted
+
+	ProxySet lbmethod=byrequests
+
+	</Proxy>
+	ProxyPass  '/' 'balancer://static-cluster/'
+	ProxyPassReverse '/' 'balancer://static-cluster/'
 </VirtualHost>
